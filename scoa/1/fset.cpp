30336d6ab849dc6d1acdcd631e4f90169d877fdf
ima.n00b <hemantd983@gmail.com>
2021-03-03 23:16:12 +0530

    fuzzyset_basic_oprations_completed

--- scoa/1/fset.cpp
+++ scoa/1/fset.cpp
@@ -23,8 +23,15 @@ auto acceptSet(int n) {
     map<int, float> A;
 
     fo(i,n+1,1) {
-        float temp; cin>>temp;
-        A[i] = temp;
+        float temp; 
+        cout<<"Enter Membership value for element ["<<i<<"] : ";
+        cin>>temp;
+        if(temp<=1)
+            A[i] = temp;
+        else {
+            cout<<"warning!!! membership value must be less than 1\n";
+            cout<<"re-enter the value again for ["<<i<<"] element : "; cin>>temp;
+        }
     }
 
     return A;
@@ -98,21 +105,97 @@ int main() {
 
     int n; 
     cout<<"\nEnter the size of set : "; cin>>n;
+    cout<<"Enter the elements of set A : ";
     map<int, float> A = acceptSet(n);
+    cout<<"Enter the elements of set B : ";
     map<int, float> B = acceptSet(n);
 
-
-    cout<<"==================================================\n";
-    cout<<"Union Set : "; printSet(unionSet(A,B)); 
-    cout<<"Intersection Set : "; printSet(intersectionSet(A,B));
-    cout<<"Complement of A : "; printSet(complementSet(A)); 
-    cout<<"Complement of B : "; printSet(complementSet(B));
     cout<<"A : ";
     printSet(A);
 
     cout<<"B : ";    
     printSet(B);
-    cout<<"A-B : "; printSet(differenceSet(A,complementSet(B)));   
-    cout<<"B-A : "; printSet(differenceSet(B,complementSet(A)));   
+
+    while(true) {
+
+        cout<<"+--------------------------------------------+\n";
+        cout<<"|*********** Fuzzy Set Operations ***********|\n";
+        cout<<"+-------+---------------------------+--------+\n";
+        cout<<"|       | *** Enter your choice *** |        |\n";
+        cout<<"|       +---------------------------+        |\n";
+        cout<<"|         1. Union [ A ∪ B ]                 |\n";
+        cout<<"|         2. Intersection [ A ∩ B ]          |\n";
+        cout<<"|         3. Difference [ A - B ]            |\n";
+        cout<<"|         4. Difference [ B - A ]            |\n";
+        cout<<"|         5. Complement of A                 |\n";
+        cout<<"|         6. Complement of B                 |\n";
+        cout<<"|         7. Change set A                    |\n";
+        cout<<"|         8. change set B                    |\n";
+        cout<<"|         9. Exit the program                |\n";
+        cout<<"+--------------------------------------------+\n";
+
+        int ch; 
+        cout<<"Enter your choice : ";
+        cin>>ch;
+
+        clearScreen;
+        cout<<"A : ";
+        printSet(A);
+
+        cout<<"B : ";    
+        printSet(B);
+
+        cout<<"\n\n*** Output ***\n";
+        cout<<"\n----------------\n"; 
+        switch (ch)
+        {
+        case 1:
+            cout<<"Union Set : "; printSet(unionSet(A,B)); 
+            break;
+        
+        case 2:
+            cout<<"Intersection Set : "; printSet(intersectionSet(A,B));
+            break;
+
+        case 3:
+            cout<<"A-B : "; printSet(differenceSet(A,complementSet(B)));
+            break;
+
+        case 4:
+            cout<<"B-A : "; printSet(differenceSet(B,complementSet(A)));
+            break;
+
+        case 5:
+            cout<<"Complement of A : "; printSet(complementSet(A)); 
+            break;
+
+        case 6:
+            cout<<"Complement of B : "; printSet(complementSet(B)); 
+            break;
+        
+        case 7:
+            cout<<"*** Update A set ***\n\n";
+            A = acceptSet(n); cout<<endl;
+            break;
+            
+        case 8:
+            cout<<"*** Update B set ***\n\n";
+            B = acceptSet(n); cout<<endl;
+            break;
+
+        case 9:
+            clearScreen;
+            cout<<"Exiting ......\n....\n..\n.\n";
+            exit(0);
+            break;
+        
+        
+        default:
+            break;
+        }
+        cout<<"\n\n\n";
+    }
+
+    return 0;
 
 }
